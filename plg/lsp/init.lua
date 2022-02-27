5e3e4d7a09550ae1932b9faecb1fc8569bbafa0b
thefirstminute <48491547+thefirstminute@users.noreply.github.com>
2022-02-27 17:16:35 -0500

    Delete plg directory

--- plg/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "plg.lsp.lsp-installer"
-require("plg.lsp.handlers").setup()
-require "plg.lsp.null-ls"
